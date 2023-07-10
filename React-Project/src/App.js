4b3e7eee0c7cc98d5e3088c665965e10f8d92fd7
aniljos <anil.jos@gmail.com>
2023-07-10 13:40:04 +0530

    Day 1

---
+++ React-Project/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function App(){
+
+    return(
+        <div>
+            <h3>React Application</h3>
+            <p>Created using Babel and Webpack</p>
+        </div>
+    )
+}
+
+export default App;
