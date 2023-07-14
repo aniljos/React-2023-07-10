1bb405a2cdf3fce5f22eb55d2fdb54f9993d0e29
aniljos <anil.jos@gmail.com>
2023-07-14 10:56:25 +0530

    Day 4

--- the-awesome-app/src/components/EditProduct.tsx
+++ the-awesome-app/src/components/EditProduct.tsx
@@ -1,4 +1,4 @@
-import React, {useEffect, useState} from "react";
+import React, {ChangeEvent, useEffect, useState} from "react";
 import {useParams} from 'react-router-dom';
 import axios from 'axios';
 import { Product } from "../models/Product";
@@ -6,7 +6,7 @@ import { Product } from "../models/Product";
 export default function EditProduct(){
 
     const params = useParams();
-    const [product, setProduct] = useState<Product>();
+    const [product, setProduct] = useState<Product>(new Product());
 
     useEffect(() => {
 
@@ -17,18 +17,45 @@ export default function EditProduct(){
     async function fetchProduct(){
         try {
             const resp = await axios.get<Product>("http://localhost:9000/products/" + params.id);
+            setProduct(resp.data);
 
       } catch (error) {
           
       }
     }
 
-
+    function handleNameChange(evt: ChangeEvent<HTMLInputElement>){
+        
+        const value = evt.target.value;
+        const copy = {...product};
+        copy.name = value;
+        setProduct(copy);
+    }
 
 
     return (
         <div>
             <h4>Edit Product: {params.id}</h4>
+
+            <div className="form-group">
+                <label>Name</label>
+                <input className="form-control" type="text" value={product.name} onChange={handleNameChange}/>
+            </div>
+
+            <div className="form-group">
+                <label>Price</label>
+                <input className="form-control" type="number" value={product.price}/>
+            </div>
+
+            <div className="form-group">
+                <label>Description</label>
+                <input className="form-control" type="text" value={product.description}/>
+            </div>
+
+            <div>
+                <button className="btn btn-success">Save</button> &nbsp;
+                <button className="btn btn-danger">Cancel</button>
+            </div>
         </div>
     )
 }
--- the-awesome-app/src/models/Product.ts
+++ the-awesome-app/src/models/Product.ts
@@ -1,13 +1,18 @@
 export class Product{
 
-    id: number;
-    name: string;
-    price: number;
-    description: string;
-    imageUrl: string;
+    id: number | undefined;
+    name: string | undefined;
+    price: number | undefined;
+    description: string | undefined;
+    imageUrl: string | undefined;
 
+    //decclartions
+    // constructor();
+    // constructor(id: number, name: string);
+    // constructor(id: number, name: string, price: number, description: string, imageUrl: string)
 
-    constructor(id: number, name: string, price: number, description: string, imageUrl: string){
+    //implementation
+    constructor(id?: number, name?: string, price?: number, description?: string, imageUrl?: string){
         
         this.id = id;
         this.name = name;
